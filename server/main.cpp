371ef663ef9c828ce2284fe6ccd9e309406c9957
Fizo55 <hugomacees56000@gmail.com>
2024-09-21 23:41:49 +0200

    [FIX] Package manager
    Delete server files for now (except main) and rework package manager using cpm-make, if it doesn't exist, it will download the file from github)

--- server/main.cpp
+++ server/main.cpp
@@ -2,13 +2,5 @@
 #include "GameServer.hpp"
 
 int main() {
-    try {
-        asio::io_context io_context;
-        GameServer server(io_context, 12345);
-        io_context.run();
-    } catch (std::exception& e) {
-        std::cerr << "Exception: " << e.what() << std::endl;
-    }
-
     return 0;
 }
