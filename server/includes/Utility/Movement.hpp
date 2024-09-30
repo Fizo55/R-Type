1ddd2c3d006588fba7ac9b2c5db42721f9762bc3
Kylian TRANCHET <kylian.tranchet@epitech.eu>
2024-09-30 04:48:39 +0200

    [ADD] Movement class
    Added the movement class for represent the movement system in the game. This class includes five method, all the getters and setters for the position and acceleration and the update method. It also include one operator =. The update method adjusts the position in function of the MoveDirection and the speed given by the Accelration class.

---
+++ server/includes/Utility/Movement.hpp
@@ -0,0 +1,84 @@
+/*
+** EPITECH PROJECT, 2024
+** R-Type
+** File description:
+** Movement
+*/
+
+#pragma once
+
+#include "Timer.hpp"
+#include "Position.hpp"
+#include "Direction.hpp"
+#include "Acceleration.hpp"
+
+class Movement {
+
+    public:
+
+        // Constructor & Destructor //
+
+        /**
+         * @brief Construct a new Movement object.
+         *
+         * @param acceleration - acceleration of the entity.
+         */
+        Movement(Acceleration& acceleration, Position& pos);
+
+        /**
+         * @brief Destroy the Movement object.
+         *
+         */
+        ~Movement() = default;
+
+        // Getters //
+
+        /**
+         * @brief Get the Position of the entity.
+         *
+         * @return float - position of the entity.
+         */
+        Position& getPosition() const;
+
+        /**
+         * @brief Get the Acceleration of the entity.
+         *
+         * @return Acceleration& - acceleration of the entity.
+         */
+        Acceleration& getAcceleration() const;
+
+        // Setters //
+
+        /**
+         * @brief Set the Position of the entity.
+         *
+         * @param pos - position to set.
+         */
+        void setPosition(Position& pos);
+
+        /**
+         * @brief Set the Acceleration of the entity.
+         *
+         * @param acceleration - acceleration to set.
+         */
+        void setAcceleration(Acceleration& acceleration);
+
+        // Methods //
+
+        /**
+         * @brief Update the position of the entity.
+         *
+         * @param deltaTime - time between two frames.
+         */
+        void update(MoveDirection direction);
+
+        // Operators //
+
+        Movement& operator=(const Movement &other);
+
+    private:
+
+        Position &_position;
+        Acceleration& _acceleration;
+        Timer _timer;
+};
---
+++ server/src/Utility/Movement.cpp
@@ -0,0 +1,59 @@
+/*
+** EPITECH PROJECT, 2024
+** R-Type
+** File description:
+** Movement
+*/
+
+#include "Movement.hpp"
+
+Movement::Movement(Acceleration& acceleration, Position& pos) :
+    _position(pos), _acceleration(acceleration), _timer() {
+        _timer.start();
+    }
+
+void Movement::update(MoveDirection direction) {
+    double deltaTime = _timer.elapsed();
+    _acceleration.update(deltaTime);
+
+    float currentSpeed = _acceleration.getCurrentSpeed();
+    switch (direction) {
+        case MoveDirection::UP:
+            _position.setY(_position.getY() - currentSpeed * deltaTime);
+            break;
+        case MoveDirection::DOWN:
+            _position.setY(_position.getY() + currentSpeed * deltaTime);
+            break;
+        case MoveDirection::LEFT:
+            _position.setX(_position.getX() - currentSpeed * deltaTime);
+            break;
+        case MoveDirection::RIGHT:
+            _position.setX(_position.getX() + currentSpeed * deltaTime);
+            break;
+    }
+    _timer.reset();
+}
+
+Position& Movement::getPosition() const {
+    return _position;
+}
+
+Acceleration& Movement::getAcceleration() const {
+    return _acceleration;
+}
+
+void Movement::setPosition(Position& pos) {
+    _position = pos;
+}
+
+void Movement::setAcceleration(Acceleration& acceleration) {
+    _acceleration = acceleration;
+}
+
+Movement& Movement::operator=(const Movement &other) {
+    if (this != &other) {
+        this->_acceleration = other._acceleration;
+        this->_position = other._position;
+    }
+    return *this;
+}
