f46924daf1eb639e3aba9daea8d39e4520336da5
Phuitric <pierre.huitric@epitech.eu>
2024-10-09 15:16:51 +0200

    [ADD] Nixos configuration file

--- flake.nix
+++ flake.nix
@@ -1,11 +1,49 @@
+# {
+#   description = "Flake config for rtype project";
+
+#   inputs = {
+#     nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+#   };
+
+#   outputs = { self, nixpkgs, ... }:
+#     let
+#       system = "x86_64-linux";
+#     in {
+#       devShells."${system}".default = let
+#         pkgs = import nixpkgs {
+#           inherit system;
+#         };
+#       in pkgs.mkShell {
+#         packages = with pkgs; [
+#           gcc
+#           cmake
+#           SDL
+#           SDL_image
+#           SDL2
+#           SDL2_image
+#           pkg-config
+#           ninja
+#           yaml-cpp
+#           asio
+#         ];
+#         shellHook = ''
+#           echo Custom C++ nix-shell!
+#           echo -e "\e[97m"
+#         '';
+#       };
+#     };
+# }
+
 {
-  description = "Flake config for rtype project";
+  description = '':
+    Flake config for rtype project
+  '';
 
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-24.05";
   };
 
-  outputs = { self, nixpkgs, ... }:
+  outputs = { self , nixpkgs ,... }:
     let
       system = "x86_64-linux";
     in {
@@ -15,7 +53,11 @@
         };
       in pkgs.mkShell {
         packages = with pkgs; [
+          bashInteractiveFHS
+          pkg-config
+
           gcc
+          valgrind
           cmake
           SDL
           SDL_image
@@ -26,9 +68,9 @@
           yaml-cpp
           asio
         ];
+
         shellHook = ''
-          echo Custom C++ nix-shell!
-          echo -e "\e[97m"
+          echo Custom cpp nix-shell !
         '';
       };
     };
